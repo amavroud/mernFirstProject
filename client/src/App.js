3111552fc7dec15d554fd131838efea3b3db28c9
Andreas Mavroudis <48466716+amavroud@users.noreply.github.com>
2022-05-25 00:57:45 -0400

    first committ
    mern project

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+//We use route in order to define the different rotues of our applicaiton
+import { Route, Routes } from "react-router";
+
+//import all the componenets we need in our app
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<RecordList />} />
+                <Route path="/edit/:id" element={<Edit />} />
+                <Route path="/create" element={<Create />} />
+            </Routes>
+        </div>
+    );
+};
+
+export default App;
